d56888b6494923772c92d92c065474c3759f7267
Josh Hattan <joshhattan@gmail.com>
2011-10-07 19:05:53 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shuttle::Application.config.secret_token = '6910d1c4daec048e45c8603b7d25b3ab0bd597e9059e34c7aa8b321ff6218299a61e07a41c8b65cb6c74a62f12bfc0fc2bcbbd8814b8e678d6f70b6d4771816c'
